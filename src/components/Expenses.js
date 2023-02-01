6790d6b6c79ff2bd6029be38cd7f2328691136e0
PavankumarMula <pavanpappy7@gmail.com>
2023-02-01 15:40:08 +0530

    refresh Added

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Expenses.css'
 
 const Expenses = ()=>{
@@ -6,11 +6,49 @@ const Expenses = ()=>{
     const[expenseName,setExpenseName]=useState('')
     const[expenseCategory,setExpenseCategory]=useState("")
     const[expenses,setExpenses]=useState([])
-    const expensesFormHandler =(event)=>{
+    useEffect(()=>{
+        try {
+            fetch(`https://react-92f28-default-rtdb.firebaseio.com/Expenses.json`)
+            .then(response=>response.json().then(data=>{
+                let myarray=[]
+                for(let key of Object.keys(data)){
+                    myarray.push(data[key])
+                }
+                setExpenses(myarray)
+            })).catch(err=>console.log(err))
+        } catch (error) {
+            alert(error);
+        }
+    },[])
+    const expensesFormHandler =async (event)=>{
         event.preventDefault();
-        setExpenses((prevvalue)=>{
-            return [...prevvalue,{eamount:expenseAmount,ename:expenseName,ecategory:expenseCategory}]
-        })
+       try {
+           const expenseResposnse=await fetch(`https://react-92f28-default-rtdb.firebaseio.com/Expenses.json`,
+            {
+                method:'POST',
+                body: JSON.stringify({
+                  amount:expenseAmount,
+                  name:expenseName,
+                  category:expenseCategory
+                }),
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+            })//fetch ends)
+            if(expenseResposnse.ok){
+                const response=await expenseResposnse.json();
+                
+                setExpenses(prevValues=>{
+                    return [...prevValues,{amount:expenseAmount,name:expenseName,category:expenseCategory}]
+                })
+            }else{
+                const response=await expenseResposnse.json();
+                throw response.error;
+            }
+       } catch (error) {
+                console.log(error.message);
+       }
+       
     }
     return <>
         <div className="card">
@@ -42,9 +80,9 @@ const Expenses = ()=>{
         <tbody>
           {expenses.map((expense, index) => (
             <tr key={index}>
-              <td>{expense.ename}</td>
-              <td>{expense.eamount}</td>
-              <td>{expense.ecategory}</td>
+              <td>{expense.name}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.category}</td>
             </tr>
           ))}
         </tbody>
